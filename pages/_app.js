524164e86981945cdd9dea1ecc0399f9fac82398
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-02-10 15:29:52 -0400

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
