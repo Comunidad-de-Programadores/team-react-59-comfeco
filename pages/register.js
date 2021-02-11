5817183c0f6b2fcb3de3d918b6edb98d13bac047
genarogg <genarrogg@gmail.com>
2021-02-11 07:11:57 -0400

    registro

---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/general/Layout";
+import Register from "../components/forms/Register";
+
+const register = () => {
+  return (
+    <Layout>
+      <main>
+        <Register />
+      </main>
+    </Layout>
+  );
+};
+
+export default register;
