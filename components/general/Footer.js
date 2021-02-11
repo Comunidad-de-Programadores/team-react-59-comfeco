5c42ff40dc7d755bfe00869fe439505bc410e405
genarogg <genarrogg@gmail.com>
2021-02-11 07:08:10 -0400

    Layout general

---
+++ components/general/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Comfeco | Team react #59 | 2021 | Footer de prueba</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
