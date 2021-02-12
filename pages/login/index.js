1237fdb736fda530e3a789e2d4a790f54ab9f6bc
Jarrison Cano <jarrison.cano@hotmail.com>
2021-02-12 10:36:04 -0500

    Issue: Layout. Close #2

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,17 +5,23 @@ export default function login(){
         <div className="form-login-container">
             <form className="row" action="">
                 <div className="row col-xs-12 input-container">
-                    <label htmlFor="loginCorreo" className="ico-background col-xs-2">
+                    <label htmlFor="loginCorreo" className="ico-background col-xs-1">
                             <span className="ico icon-mail-envelope-closed"></span>
                     </label>
-                    <input name="loginCorreo" id="loginCorreo" className="col-xs-10" type="email" placeholder="Correo" />
+                    <input name="loginCorreo" id="loginCorreo" className="col-xs-11" type="email" placeholder="Correo electrónico" autoComplete="off"/>
                 </div>
                 <br/>
                 <div className="row col-xs-12 input-container">
-                    <label htmlFor="loginPassword" className="ico-background col-xs-2">
+                    <label htmlFor="loginPassword" className="ico-background col-xs-1">
                             <span className="ico icon-lock"></span>
                     </label>
-                    <input name="loginPassword" id="loginPassword" className="col-xs-10" type="password" placeholder="Contraseña" />
+                    <input name="loginPassword" id="loginPassword" className="col-xs-11" type="password" placeholder="Contraseña" />
+                </div>
+                <br/>
+                <div className="button-container col-xs-12">
+                    <button name="loginButton" id="loginButton" type="submit" value="send">
+                        Iniciar sesión
+                    </button>   
                 </div>
                 <br/>
                 <div className="forgotPassword-container col-xs-12">
@@ -23,14 +29,6 @@ export default function login(){
                         <a className="link">¿Olvidaste tu contraseña?</a>
                     </Link>
                 </div>
-                <br/>
-                <div className="button-container col-xs-12">
-                    <button name="loginButton" id="loginButton" className="col-xs-12" type="submit" value="send">
-                        Acceder
-                    </button>   
-                </div>
-                <br/>
-                <hr className="col-xs-12"/>
             </form>
         </div>
     )
