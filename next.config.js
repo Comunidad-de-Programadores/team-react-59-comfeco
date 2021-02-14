0e724f2881b77712c60839d1d354ddef93d720c1
genarogg <genarrogg@gmail.com>
2021-02-14 03:41:06 -0400

    imgs

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ const path = require('path')
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+    
+  },
+  images: {
+    deviceSizes: [256, 384, 640, 750, 828, 1080, 1200, 1920, 2048],
   },
 }
