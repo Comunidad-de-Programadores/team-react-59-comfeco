fc4eaa76c6a898725b10f39a3e95442f1c2f33f8
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-02-14 15:10:36 -0400

    register fix v3

--- pages/api/register.js
+++ pages/api/register.js
@@ -62,7 +62,7 @@ export default async function handler(req,res){
                 const token = await user.user.getIdTokenResult();
                 
                 //Request successful
-                res.status(200).json({ token });
+                res.status(200).json({ token: token.token });
             })
             .catch(error => {
 
