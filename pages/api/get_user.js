1291030a04a82e7e50c235bc62aba138fbabc6d7
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-02-14 15:48:53 -0400

    Get User added

--- pages/api/get_user.js
+++ pages/api/get_user.js
@@ -1,4 +1,7 @@
 import admin from "firebase-admin";
+import firebase from "firebase";
+
+const db = () => firebase.firestore()
 
 export const config = {
   api: {
@@ -28,18 +31,38 @@ export default async function handler(req, res) {
             appId: "1:527568472092:web:342dea10fc14f14fd677d9"
         };
 
-        //Veify if there is apps initialized of firebase
+        //Veify if there is apps initialized of firebase admin
         if(!admin.apps.length){
             admin.initializeApp(firebaseConfig);
         }else{
             admin.app();
         }
 
+        //Veify if there is apps initialized of firebase
+        if(!firebase.apps.length){
+            firebase.initializeApp(firebaseConfig);
+        }else{
+            firebase.app();
+        }
+
         admin.auth().verifyIdToken(req.headers.authorization)
-            .then(user => {
-                res.status(200).json({user})
+            .then(async (user) => {
+
+                //Search the nickname
+                const data = await db().collection("users").where("user","==",user.uid).get()
+                let nickname;
+                data.forEach(v => {nickname = v.data().nickname})
+
+                //Request successfull
+                res.status(200).json({
+                    uid: user.uid,
+                    email: user.email,
+                    nickname
+                })
             })
             .catch(error => {
+
+                //Request failed
                 res.status(200).json({error})
             })
     }
