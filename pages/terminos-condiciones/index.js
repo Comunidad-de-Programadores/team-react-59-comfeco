0ee6c9a7ef084b26261fc001622886f12ab570cc
MoisesLagos <moises180688@gmail.com>
2021-02-14 20:35:31 -0500

    se añadió terminos y condiciones

---
+++ pages/terminos-condiciones/index.js
@@ -0,0 +1,67 @@
+
+
+export default function TerminosCondiciones(){
+    return (
+        <>
+            <h1>TÉRMINOS DEL SERVICIO DE COMFECO</h1>
+            <p>Fecha: 12 de Febrero de 2021 | <a href="https://www.google.com" target="_blank">Descargar PDF</a></p>
+            <br/>
+            <h2>Qué se trata en estos términos</h2>
+            <h3>Sabemos que es tentador saltarse estos Términos del Servicio, pero es importante determinar qué puedes esperar de nosotros al usar nuestros talleres y qué esperamos nosotros de ti.</h3>
+            <p>
+            Estos Términos del Servicio reflejan la forma de trabajar de Comfeco como empresa, las leyes por las que nos regimos y determinados aspectos que siempre hemos creído que son ciertos. Por ello, estos Términos ayudan a definir la relación que tiene #Comfeco contigo cuando interactúas con nuestros servicios. Por ejemplo, estos términos incluyen las siguientes secciones:
+            </p>
+
+            <ul>
+                <li>Qué puedes esperar de nosotros, donde se describe cómo proporcionamos y desarrollamos nuestros servicios.</li>
+                <li>Lo que esperamos de ti, donde se establecen ciertas reglas para utilizar nuestros servicios.</li>
+                <li>En caso de problemas o discrepancias, donde se describen otros derechos legales que tienes y las consecuencias de infringir estos términos.</li>
+            </ul>
+
+            <p>
+            Es importante que entiendas estos términos porque el uso de nuestros servicios implica que los aceptas.
+            </p>
+            <p>
+            Además de estos términos, también publicamos una Política de Privacidad. Aunque no forme parte de estos términos, te recomendamos que la leas para entender mejor cómo puedes actualizar, gestionar, exportar y eliminar tu información.
+            </p>
+
+            <h2>Requisitos de edad mínima</h2>
+            <p>
+            Si no tienes la edad mínima para poder gestionar tu propia cuenta de Comfeco, debes contar con el permiso de tu padre, madre o tutor legal para utilizar una cuenta de Comfeco. Pide a tu padre, madre o tutor legal que lea estos términos contigo.
+            </p>
+            <p>
+            Si eres padre, madre o tutor legal y permites a tu hijo/a que utilice los servicios, se te aplican estos términos y eres responsable de la actividad que realice en los servicios.
+            </p>
+
+            <style jsx>{`
+            h1{
+                color: #3c4043;
+                display: inline-block;
+                font-size: 12px;
+                font-weight: 500;
+                letter-spacing: .8px;
+                line-height: 20px;
+                margin: 1.236em 0 .618em 0;
+                text-transform: uppercase;
+            }
+            h2{
+                font-size: 22px;
+                line-height: 28px;
+                font-weight: 500;
+                padding: 64px 0 0 0;
+            }
+            h3{
+                font-size: 24px;
+                line-height: 32px;
+                padding-top: 24px;
+                padding-bottom: 4px;
+            }
+            p{
+                letter-spacing: .2px;
+                line-height: 24px;
+                padding-top: 20px;
+            }
+            `}</style>
+        </>
+    )
+}
