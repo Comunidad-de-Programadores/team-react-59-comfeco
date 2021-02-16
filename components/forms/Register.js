66cff348e99078095acd0fd3472655ee983f3994
genarogg <genarrogg@gmail.com>
2021-02-16 11:09:35 -0400

    Register.js

--- components/forms/Register.js
+++ components/forms/Register.js
@@ -1,100 +1,86 @@
 import React from "react";
 import Icono from "../nano/Icono";
 import A from "../nano/A";
-import $ from "../nano/$"
+import Buttons from "./Buttons";
 
 const Register = () => {
-  const voltearIniciar = () => {
-    const tarjeta = $("containerRegisterLogin")
-  
-    if(tarjeta.classList.contains('active')){
-      tarjeta.classList.remove('active');
-    }
-    
-  }
   return (
-    <div className="trasera formGroupSesion col-xs-5">
-      <div className="botonesDeSesion formRegisterButton">
-        <button className="iniciarSesion" onClick={() => {
-              voltearIniciar();
-            }}>
-          <A href="#iniciarSesion">Iniciar sesión</A>
-        </button>
-        <span>|</span>
-        <button className="registrarse">
-          <A href="#registrarse">Regístrarse</A>
-        </button>
-      </div>
+    <div /*  */
+      className="traseraDerecha formGroupSesion col-xs-5 "
+      id="traseraDerecha"
+    >
+      <Buttons />
       <form action="" className="row">
         <div className="row col-xs-12 containerInput">
-          <span className="icoBackground col-xs-2">
+          <label htmlFor="registerNombre" className="icoBackground col-xs-1">
             <span className="ico icon-user"></span>
-          </span>
-          <input
-            type="text"
-            placeholder="Nombre de Usuario"
-            maxLength="15"
-            id="registerUserName"
-            name="registerUserName"
-            className="col-xs-10"
-          />
-        </div>
-        <br />
-        <div className="row col-xs-12 containerInput">
-          <span className="icoBackground col-xs-2">
-            <span className="ico icon-user"></span>
-          </span>
+          </label>
           <input
             type="text"
             placeholder="Nombre Completo"
             maxLength="24"
             id="registerNombre"
             name="registerNombre"
-            className="col-xs-10"
+            className="col-xs-11"
           />
         </div>
         <br />
-
         <div className="row col-xs-12 containerInput">
-          <span className="icoBackground col-xs-2">
+          <label htmlFor="registerUserName" className="icoBackground col-xs-1">
+            <span className="ico icon-person_add_alt_1"></span>
+          </label>
+          <input
+            type="text"
+            placeholder="Nombre de Usuario"
+            maxLength="15"
+            id="registerUserName"
+            name="registerUserName"
+            className="col-xs-11"
+          />
+        </div>
+        <br />
+        <div className="row col-xs-12 containerInput">
+          <label htmlFor="registerCorreo" className="icoBackground col-xs-1">
             <span className="ico icon-mail-envelope-closed"></span>
-          </span>
+          </label>
           <input
             type="email"
             placeholder="Correo"
             name="registerCorreo"
             id="registerCorreo"
-            className="col-xs-10"
+            className="col-xs-11"
           />
         </div>
         <br />
         <div className="row col-xs-12 containerInput">
-          <span className="icoBackground col-xs-2">
-            <span className="ico icon-lock1"></span>
-          </span>
+          <label htmlFor="registerPassword" className="icoBackground col-xs-1">
+            <span className="ico icon-https"></span>
+          </label>
           <input
             type="password"
             name="registerPassword"
             id="registerPassword"
             placeholder="Contraseña"
-            className="col-xs-10"
+            className="col-xs-11"
           />
         </div>
         <br />
         <div className="row col-xs-12 containerInput">
-          <span className="icoBackground col-xs-2">
-            <span className="ico icon-lock"></span>
-          </span>
+          <label
+            htmlFor="registerPasswordConfirm"
+            className="icoBackground col-xs-1"
+          >
+            <span className="ico icon-enhanced_encryption"></span>
+          </label>
           <input
             type="password"
             name="registerPasswordConfirm"
             id="registerPasswordConfirm"
             placeholder="Confirmar Contraseña"
-            className="col-xs-10"
+            className="col-xs-11"
           />
         </div>
 
-       
         <div className="containerRegistrarse">
           <button
             className="registerButtom"
@@ -107,9 +93,10 @@ const Register = () => {
           </button>
         </div>
       </form>
-        <div className="legal">
+      <div className="legal">
         <p>
-          Al registrarte, estas aceptando los <A>Términos y condiciones</A>, y la 
+          Al registrarte, estas aceptando los <A>Términos y condiciones</A>, y
+          la
           <A> Política de privacidad y protección de datos</A> de COMFECO.
         </p>
       </div>
