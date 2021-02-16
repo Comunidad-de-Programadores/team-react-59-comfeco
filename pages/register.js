a4a76812d8e837ce82fa2153c62d77fd40921f5b
genarogg <genarrogg@gmail.com>
2021-02-16 11:10:03 -0400

    Invocando todos los elementos del login

--- pages/register.js
+++ pages/register.js
@@ -2,16 +2,14 @@ import React from "react";
 import Layout from "../components/general/Layout";
 import Register from "../components/forms/Register";
 import Login from "../components/forms/Login";
+import ResetPassword from "../components/forms/ResetPassword";
 import Image from "next/Image"
 
 const register = () => {
- 
-  
-
   return (
     <Layout>
-      <div className="backgroundRegister"></div>
-      <div className="containerSesion row between-xs">
+      <div className="backgroundRegister "></div>
+      <div className="containerSesion row between-xs ">
         <div className="col-xs-5">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
@@ -20,10 +18,14 @@ const register = () => {
             veritatis praesentium doloremque corrupti.
           </p>
         </div>
-        <div className="containerRegisterLogin" id="containerRegisterLogin">
-        <Login />
-        <Register />
-         {/* <Image src="/Paleta.png" width={200} height={100} /> */}
+        <div className="containerRegisterLogin " id="containerRegisterLogin">
+        <ResetPassword />
+        
+          <Login />
+
+          <Register />
+          
+          
         </div>
       </div>
     </Layout>
