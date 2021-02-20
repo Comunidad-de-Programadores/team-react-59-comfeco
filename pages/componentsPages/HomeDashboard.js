ed50082c3f9c484eb9dc7e2810008caf3ca810b5
GlisseLisbeth <glisselisbeth@gmail.com>
2021-02-20 18:56:16 -0500

    refactoring Workshop

--- pages/componentsPages/HomeDashboard.js
+++ pages/componentsPages/HomeDashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Communities from '../../components/card/CardCommunity/Communities';
+import Workshops from '../../components/card/CardCommunity/Workshops';
 
 const HomeDashboard = () => {
   return (
@@ -19,65 +20,7 @@ const HomeDashboard = () => {
         <div className="panelSponsor">Sponsors</div>
         <div className="panelTimer">Timer</div>
         <div className="panelTaller">
-          <div class="card-container-taller">
-            <div className="card-taller">
-              <div className="card-taller-header">
-                <span className="card-taller-title"> Talleres</span>
-                <a className="card-taller-link"><span>Ver más</span></a>
-              </div>
-              <div className="card-taller-select">
-                <select className="select-taller">
-                    <option>Talleres por área de conocimiento</option>
-                    <option>Primera opción</option>
-                    <option>Segunda opción</option>
-                    <option>Tercera opción</option>
-                    <option>Cuarta opción</option>
-                </select>
-              </div>
-              <div className="card-taller-event">
-                  Hoy
-              </div>
-              <div className="card-taller-body">
-                <div className="card-taller-content">
-                  <div className="card-taller-img">
-                    <svg width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.45508 8.64039L8.80273 14.988L21.4988 2.29199" stroke="black" stroke-width="4" stroke-linecap="round"/>
-                    </svg>
-                  </div>
-                  <div className="card-taller-text">
-                    <span className="card-taller-title">State of Javascript</span>
-                    <span className="card-taller-hora">16:00 PM</span>
-                    <span className="card-taller-subtext">By<span className="card-taller-name">Juan Pablo de la Torre</span></span>
-                  </div>
-                </div>
-              </div>
-              <div className="card-taller-body">
-                <div className="card-taller-content">
-                  <div className="card-taller-img">
-                    <svg width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.45508 8.64039L8.80273 14.988L21.4988 2.29199" stroke="black" stroke-width="4" stroke-linecap="round"/>
-                    </svg>
-                  </div>
-                  <div className="card-taller-text">
-                    <span className="card-taller-title">State of Javascript</span>
-                    <span className="card-taller-hora">16:00 PM</span>
-                    <span className="card-taller-subtext">By<span className="card-taller-name">Juan Pablo de la Torre</span></span>
-                  </div>
-                </div>
-              </div>
-              <div className="card-taller-body">
-                <div className="card-taller-content">
-                  <div className="card-taller-img">
-                    <svg width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.45508 8.64039L8.80273 14.988L21.4988 2.29199" stroke="black" stroke-width="4" stroke-linecap="round"/>
-                    </svg>
-                  </div>
-                  <div className="card-taller-text">
-                    <span className="card-taller-title">State of Javascript</span>
-                    <span className="card-taller-hora">16:00 PM</span>
-                    <span className="card-taller-subtext">By<span className="card-taller-name">Juan Pablo de la Torre</span></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          < Workshops />  
         </div>
       </div>
     </main>
