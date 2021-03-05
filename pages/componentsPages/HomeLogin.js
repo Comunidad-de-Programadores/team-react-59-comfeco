9db9b1dc0756b3591ef5b848b390a2c31e093d8a
genarogg <genarrogg@gmail.com>
2021-03-05 00:37:33 -0400

    login redes sociales

--- pages/componentsPages/HomeLogin.js
+++ pages/componentsPages/HomeLogin.js
@@ -1,66 +1,37 @@
 import React from "react";
+import HomeDashboardLogin from "./HomeDashboardLogin";
 import Register from "../../components/forms/Register";
 import Login from "../../components/forms/Login";
 import ResetPassword from "../../components/forms/ResetPassword";
 
 const HomeLogin = () => {
   return (
-   <>
-      {/* <div className="backgroundRegister "></div> */}
-      <div className="containerSesion row between-md center-xs">
-        <div className="containerText col-xs-12 col-md-5 ">
-          <div className="title">
-            <h4>Sobre el evento</h4>
-            <hr />
+    <>
+      <main>
+        <div className="backgroundHome"></div>
+        <div className="backgroundLigth"></div>
+        <div className="containerSesion row between-md center-xs">
+          <div className="welcome row col-xs-12 col-sm-6  col-lg-4 center-xs">
+            <h1>Bienvenidos comfeco ¡Community Fest and Code!</h1>
+            <h3>¡Conoce gente, aprende y gana!</h3>
+            <div className="text">
+              <p>
+                La próxima edición regresa en el 2022, en la cual se planea
+                involucrar a todos los programadores independientmente del area
+                de conocimiento que se encuentre, todo con un mismo proposito,
+                aprender en comunidad.
+              </p>
+            </div>
           </div>
-          <div className="containerP row">
-            <span className="ico icon-check-circle col-xs-2 col-md-1"></span>
-            <p className="col-xs-10 col-md-11">
-              Los grupos tienen que ser conformados por 5 personas.
-            </p>
-          </div>
-          <div className="containerP row">
-            <span className="ico icon-check-circle col-xs-2 col-md-1"></span>
-            <p className="col-xs-10 col-md-11">
-              Si estas en el Team Angular no podrás presentar el proyecto en un
-              framework distinto, así para cada uno de los casos.
-            </p>
-          </div>
-          <div className="containerP row">
-            <span className="ico icon-check-circle col-xs-2 col-md-1"></span>
-            <p className="col-xs-10 col-md-11">
-              Se debe entregar el proyecto en la fecha indicada, no se dará tiempo
-              extra, por eso la fecha se les dirá con mucha anticipación.
-            </p>
-          </div>
-          <div className="containerP row">
-            <span className="ico icon-check-circle col-xs-2 col-md-1"></span>
-            <p className="col-xs-10 col-md-11">
-              Se debe seguir las directrices para hacer el proyecto, y se debe de
-              cumplir con cada uno de los requisitos para que puedan ser
-              aprobados.
-            </p>
-          </div>
-          <div className="containerP row">
-            <span className="ico icon-check-circle col-xs-2 col-md-1"></span>
-            <p className="col-xs-10 col-md-11">
-              Los lideres de cada team no realizaran el proyecto por ti, solo
-              estarán como guía para dar algunos talleres y resolver dudas para
-              poder completar con éxito el proyecto.
-            </p>
-          </div>
-          <div className="containerP row pRelleno">
-            <span className="col-xs-1"></span>
-            <p className="col-xs-10 col-md-11"></p>
+          <div className="containerForm  " id="containerRegisterLogin">
+            <ResetPassword />
+            <Login />
+            <Register />
           </div>
         </div>
-
-        <div className="containerForm  " id="containerRegisterLogin">
-          <ResetPassword />
-          <Login />
-          <Register />
-        </div>
-      </div>
+        <HomeDashboardLogin />
+      </main>
+     
     </>
   );
 };
