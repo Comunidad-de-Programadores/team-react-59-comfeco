c3d9496383a451d6f4a7e8fe90aaa98ebb2a31e2
genarogg <genarrogg@gmail.com>
2021-03-08 20:04:12 -0400

    finalizando el responsiv de el dashboard

--- pages/componentsPages/home/Home.js
+++ pages/componentsPages/home/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
         <main>
           <div className="backgroundHome"></div>
           <div className="backgroundLigth"></div>
-          <div className="containerSesion row center-xs">
+          <div className="containerSesion logueado row center-xs">
             <div className="welcome row col-xs-12 center-xs">
-              <h1>Bienvenidos <b>comfeco</b> ¡Community Fest and Code!</h1>
-              <h3>¡Conoce gente, aprende y gana!</h3>
+              <h1>Bienvenidos <b>comfeco</b> <br/>¡Community Fest and Code!</h1>
+              <h3 className="col-xs-12">¡Conoce gente, aprende y gana!</h3>
               <div className="text">
                 <p>
                   La próxima edición regresa en el 2022, en la cual se planea
