7df1a1d5688e888ef46d115e5f74fdf4d6da690f
genarogg <genarrogg@gmail.com>
2021-03-09 21:55:47 -0400

    refactorizando el css parte 2

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/lib/librerias.scss";
 import "../styles/icons/style.css";
 
 /* Estilos globales, header y footer */
-import "../styles/globals/style.scss";
+import "../styles/globals/globals.scss";
 
 /* Estilos de los componentes de los formularios de registro */
 import "../styles/form/formRegisterLogin.scss";
@@ -11,6 +11,13 @@ import "../styles/form/newPassword.scss";
 import "../styles/form/loginRedes.scss";
 
 /*  */
+/* import "../styles/dashboard.scss";
+import "../styles/deadline.scss";
+import "../styles/homeLogin.scss"; */
+import "../styles/home/home.scss";
+
+
+
 /* css del slider */
 import "../styles/slider/swiper.scss";
 import "../styles/slider/estilos.scss";
@@ -18,19 +25,18 @@ import "../styles/slider/sliderSleader.scss";
 import "../styles/slider/sliderSponsors.scss";
 /* css para cambiar los colores de la interfas (Proximaente)*/
 
-import "../styles/form/newPassword.scss";
+
 import "../styles/form/editProfile.scss";
-import "../styles/dashboard.scss";
+
 
 /* Clase para meter nuevos estilos (Uxiliar) */
 import "../styles/demo.scss";
-/* import "../styles/404/404.scss"; */
 
 /* css de las paginas */
-import "../styles/homeLogin.scss";
+
 
 /* Estilos del Deadline */
-import "../styles/deadline.scss";
+
 
 /* estilos para botones */
 import "../styles/button/btn1.scss";
