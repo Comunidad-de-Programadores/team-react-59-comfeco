8e09a1c867df895709d728385c6de818cba57f36
genarogg <genarrogg@gmail.com>
2021-03-10 05:50:45 -0400

    refactorizado el css parte 3

--- pages/_app.js
+++ pages/_app.js
@@ -5,52 +5,43 @@ import "../styles/icons/style.css";
 /* Estilos globales, header y footer */
 import "../styles/globals/globals.scss";
 
+/* estilos para botones */
+import "../styles/button/btn1.scss";
+import "../styles/button/btnHamburguesa.scss";
+
 /* Estilos de los componentes de los formularios de registro */
 import "../styles/form/formRegisterLogin.scss";
 import "../styles/form/newPassword.scss";
 import "../styles/form/loginRedes.scss";
 
-/*  */
-/* import "../styles/dashboard.scss";
-import "../styles/deadline.scss";
-import "../styles/homeLogin.scss"; */
+/* Estilos del home */
 import "../styles/home/home.scss";
 
+/* Estilos del slider */
+import "../styles/slider/slider.scss";
+
+/*  */
 
 
-/* css del slider */
-import "../styles/slider/swiper.scss";
-import "../styles/slider/estilos.scss";
-import "../styles/slider/sliderSleader.scss";
-import "../styles/slider/sliderSponsors.scss";
-/* css para cambiar los colores de la interfas (Proximaente)*/
 
 
 import "../styles/form/editProfile.scss";
 
-
 /* Clase para meter nuevos estilos (Uxiliar) */
 import "../styles/demo.scss";
 
-/* css de las paginas */
 
-
-/* Estilos del Deadline */
-
-
-/* estilos para botones */
-import "../styles/button/btn1.scss";
-import "../styles/button/btnHamburguesa.scss";
 
 /* estilos perfil */
 import "../styles/perfil/miPerfil.scss";
 
-import { Provider } from "next-auth/client";
-
 import "../styles/perfil/perfil.scss";
 import "../styles/cards.scss";
 import "../styles/perfil/cardEvents.scss";
+
 import "../styles/colorChange.scss";
+
+import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   return (
