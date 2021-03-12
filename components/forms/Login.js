dcdfd3335a10e363f0861527c20a49f196704751
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-03-11 22:13:00 -0400

    fix

--- components/forms/Login.js
+++ components/forms/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Buttons from "./Buttons";
 import Router from "next/router";
-import $ from "../nano/$";
 import Icono from "../nano/Icono";
+import $ from "../nano/$";
 import RedesLogin from "./RedesLogin";
 
 const Login = () => {
