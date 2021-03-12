28d459a4d20c30ce58e2cb768dcdb876a78047d8
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-03-11 22:07:21 -0400

    fix in form

--- components/forms/RedesLogin.js
+++ components/forms/RedesLogin.js
@@ -8,7 +8,7 @@ const RedesLogin = () => {
     <div className="redesSociales">
       <ul>
         <li>
-          <button onClick={() => signIn("google")}>
+          <button type="button" onClick={() => signIn("google")}>
             <A href="#">
               <Icono css="icon-google googleHover" />
               <span></span>
@@ -16,7 +16,7 @@ const RedesLogin = () => {
           </button>
         </li>
         <li>
-          <button onClick={() => signIn("facebook")}>
+          <button type="button" onClick={() => signIn("facebook")}>
             <A href="#">
               <Icono css="icon-facebook facebookHover" />
               <span></span>
@@ -24,7 +24,7 @@ const RedesLogin = () => {
           </button>
         </li>
         <li>
-          <button onClick={() => signIn("twitter")}>
+          <button type="button" onClick={() => signIn("twitter")}>
             <A href="#">
               <Icono css="icon-twitter twitterHover" />
               <span></span>
@@ -32,7 +32,7 @@ const RedesLogin = () => {
           </button>
         </li>
         <li>
-          <button onClick={() => signIn("github")}>
+          <button type="button" onClick={() => signIn("github")}>
             <A href="#">
               <Icono css="icon-github1 githubHover" />
               <span></span>
