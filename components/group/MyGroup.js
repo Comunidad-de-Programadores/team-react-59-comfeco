fe18829b952adb6e8659294915265219e080222a
Jarrison Cano <jarrison.cano@hotmail.com>
2021-03-18 14:50:48 -0500

    Improve Groups-page Styles

--- components/group/MyGroup.js
+++ components/group/MyGroup.js
@@ -1,88 +1,109 @@
-import React from "react";
-import Image from 'next/image';
-import Link from "next/link";
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const MyGroup = (group) => {
-    return(
-        <section className="card-widget">
-            <div className="card-widget-head ">
-                <Link href="/group/team-react-59">
-                    <a className="card-flex-inline name_team">
-                        <div className="title_name">
-                            <h3>Mi grupo</h3>
-                            <h2>Team React #59 »</h2>
-                        </div>
-                        <i>
-                            <svg xmlns="http://www.w3.org/2000/svg" aria-label="JavaScript" viewBox="0 0 512 512"><rect width="512" height="512" fill="#f7df1e" rx="15%"/><path d="m324,370c10,17 24,29 47,29 20,0 33,-10 33,-24 0,-16 -13,-22 -35,-32l-12,-5c-35,-15 -58,-33 -58,-72 0,-36 27,-64 70,-64 31,0 53,11 68,39l-37,24c-8,-15 -17,-21 -31,-21 -14,0 -23,9 -23,21 0,14 9,20 30,29l12,5c41,18 64,35 64,76 0,43 -34,67 -80,67 -45,0 -74,-21 -88,-49zm-170,4c8,13 14,25 31,25 16,0 26,-6 26,-30V203h48v164c0,50 -29,72 -72,72 -39,0 -61,-20 -72,-44z"/></svg>
-                        </i>
-                    </a>
-                </Link>
+  return (
+    <section className='card-widget-2'>
+      <div className='card-widget-head '>
+        <Link href='/group/team-react-59'>
+          <a className='card-flex-inline name_team'>
+            <div className='title_name'>
+              <h3>Mi grupo</h3>
+              <h2>Team React #59 »</h2>
             </div>
-            <div className="card-widget-body card-scroll" data-size="small">
-                {/* <!--cards--> */}
-                <div className="card-group">
-                    <div className="card-group-image">
-                        <Image src="/default-event.jpg" alt="default avatar" width={50} height={50} />
-                    </div>
-                    <div className="card-group-title">
-                        <div className="card-group-title_name">Juansecu</div>
-                        <div className="card-group-title_nivel">Avanzado</div>
-                    </div>
-                    <div className="card-group-rango">
-                        Integrante
-                    </div>
-                </div>
-                <div className="card-group">
-                    <div className="card-group-image">
-                        <Image src="/default-event.jpg" alt="default avatar" width={50} height={50} />
-                    </div>
-                    <div className="card-group-title">
-                        <div className="card-group-title_name">Juansecu</div>
-                        <div className="card-group-title_nivel">Avanzado</div>
-                    </div>
-                    <div className="card-group-rango">
-                        Integrante
-                    </div>
-                </div>
-                <div className="card-group">
-                    <div className="card-group-image">
-                        <Image src="/default-event.jpg" alt="default avatar" width={50} height={50} />
-                    </div>
-                    <div className="card-group-title">
-                        <div className="card-group-title_name">Juansecu</div>
-                        <div className="card-group-title_nivel">Avanzado</div>
-                    </div>
-                    <div className="card-group-rango">
-                        Lider
-                    </div>
-                </div>
-                <div className="card-group">
-                    <div className="card-group-image">
-                        <Image src="/default-event.jpg" alt="default avatar" width={50} height={50} />
-                    </div>
-                    <div className="card-group-title">
-                        <div className="card-group-title_name">Juansecu</div>
-                        <div className="card-group-title_nivel">Avanzado</div>
-                    </div>
-                    <div className="card-group-rango">
-                        Integrante
-                    </div>
-                </div>
-                {/* <!-- end cards --> */}
-            </div>
-            <div className="card-widget-footer">
-                <div className="row middle-xs action-groups">
-                    <div className="col-xs-6">
-                        <button className="btn btn-outline-default full-width">Abandonar</button>
-                    </div>
-                    <div className="col-xs-6">
-                        <Link href="/chat">
-                            <a className="btn btn-default full-width">ir al chat</a>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+            <i>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                aria-label='JavaScript'
+                viewBox='0 0 512 512'
+              >
+                <rect width='512' height='512' fill='#f7df1e' rx='15%' />
+                <path d='m324,370c10,17 24,29 47,29 20,0 33,-10 33,-24 0,-16 -13,-22 -35,-32l-12,-5c-35,-15 -58,-33 -58,-72 0,-36 27,-64 70,-64 31,0 53,11 68,39l-37,24c-8,-15 -17,-21 -31,-21 -14,0 -23,9 -23,21 0,14 9,20 30,29l12,5c41,18 64,35 64,76 0,43 -34,67 -80,67 -45,0 -74,-21 -88,-49zm-170,4c8,13 14,25 31,25 16,0 26,-6 26,-30V203h48v164c0,50 -29,72 -72,72 -39,0 -61,-20 -72,-44z' />
+              </svg>
+            </i>
+          </a>
+        </Link>
+      </div>
+      <div className='card-widget-body card-scroll' data-size='small'>
+        {/* <!--cards--> */}
+        <div className='card-group'>
+          <div className='card-group-image'>
+            <Image
+              src='/default-event.jpg'
+              alt='default avatar'
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className='card-group-title'>
+            <div className='card-group-title_name'>Juansecu</div>
+            <div className='card-group-title_nivel'>Avanzado</div>
+          </div>
+          <div className='card-group-rango'>Integrante</div>
+        </div>
+        <div className='card-group'>
+          <div className='card-group-image'>
+            <Image
+              src='/default-event.jpg'
+              alt='default avatar'
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className='card-group-title'>
+            <div className='card-group-title_name'>Juansecu</div>
+            <div className='card-group-title_nivel'>Avanzado</div>
+          </div>
+          <div className='card-group-rango'>Integrante</div>
+        </div>
+        <div className='card-group'>
+          <div className='card-group-image'>
+            <Image
+              src='/default-event.jpg'
+              alt='default avatar'
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className='card-group-title'>
+            <div className='card-group-title_name'>Juansecu</div>
+            <div className='card-group-title_nivel'>Avanzado</div>
+          </div>
+          <div className='card-group-rango'>Lider</div>
+        </div>
+        <div className='card-group'>
+          <div className='card-group-image'>
+            <Image
+              src='/default-event.jpg'
+              alt='default avatar'
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className='card-group-title'>
+            <div className='card-group-title_name'>Juansecu</div>
+            <div className='card-group-title_nivel'>Avanzado</div>
+          </div>
+          <div className='card-group-rango'>Integrante</div>
+        </div>
+        {/* <!-- end cards --> */}
+      </div>
+      <div className='card-widget-footer'>
+        <div className='row middle-xs action-groups'>
+          <div className='col-xs-6'>
+            <button className='btn btn-outline-default full-width'>
+              Abandonar
+            </button>
+          </div>
+          <div className='col-xs-6'>
+            <Link href='/chat'>
+              <a className='btn btn-default full-width'>ir al chat</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
-export default MyGroup;
+export default MyGroup
--- pages/_app.js
+++ pages/_app.js
@@ -1,56 +1,54 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 /* estilos de las librerias */
-import "../styles/lib/librerias.scss";
-import "../styles/icons/style.css";
+import '../styles/lib/librerias.scss'
+import '../styles/icons/style.css'
 
 /* Estilos globales, header y footer */
-import "../styles/globals/globals.scss";
+import '../styles/globals/globals.scss'
 
 /* estilos para botones */
-import "../styles/button/btn2.scss";
-import "../styles/button/btnHamburguesa.scss";
+import '../styles/button/btn2.scss'
+import '../styles/button/btnHamburguesa.scss'
 
 /* Estilos de los componentes de los formularios de registro */
-import "../styles/form/formRegisterLogin.scss";
-import "../styles/form/newPassword.scss";
-import "../styles/form/loginRedes.scss";
+import '../styles/form/formRegisterLogin.scss'
+import '../styles/form/newPassword.scss'
+import '../styles/form/loginRedes.scss'
 
 /* Estilos del home */
-import "../styles/home/home.scss";
+import '../styles/home/home.scss'
 
 /* Estilos del slider */
-import "../styles/slider/slider.scss";
+import '../styles/slider/slider.scss'
 
 /* estilos perfil */
-import "../styles/perfil/miPerfil.scss";
+import '../styles/perfil/miPerfil.scss'
 /*  */
 
-import "../styles/form/editProfile.scss";
+import '../styles/form/editProfile.scss'
 
 /* Clase para meter nuevos estilos (Uxiliar) */
-import "../styles/demo.scss";
-
-
+import '../styles/demo.scss'
 
 /* import "../styles/perfil/perfil.scss"; */
-/* import "../styles/cards.scss"; */
+import '../styles/cards.scss'
 
-import "../styles/colorChange.scss";
+import '../styles/colorChange.scss'
 
-import { Provider } from "next-auth/client";
+import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <script src="/swiper.min.js"></script>
+        <script src='/swiper.min.js'></script>
       </Head>
       <Provider session={pageProps.session}>
         <Component {...pageProps} />
       </Provider>
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
