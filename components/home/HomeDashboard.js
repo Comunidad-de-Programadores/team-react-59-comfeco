0b738af256d3a152e852bdda5459da63d3a993e6
Marcelino A. Abreu Mejia <marcelinomejia2009@gmail.com>
2021-03-20 23:32:18 -0400

    fix v6

--- components/home/HomeDashboard.js
+++ components/home/HomeDashboard.js
@@ -6,12 +6,10 @@ import Sleader from "../sliders/Sleader";
 import CorouselSponsors from "../sliders/CorouselSponsors";
 
 const HomeDashboard = () => {
-  const timer = <Counter deadline={1614560400}></Counter>
+  const timer = <Counter deadline={5050505050}></Counter>;
   return (
     <>
-    
       <div className="content row col-xs-12" id="content">
-        
         <div className="panelColaborador">
           <div className="title">
             <h4>Lideres de equipo 2021</h4>
@@ -21,22 +19,16 @@ const HomeDashboard = () => {
         <div className="containerPaneles row col-xs-12 around-xs">
           <div className="panelCom">{<Communities />}</div>
 
-          <div className="panelTimer col-xs-5 timeLg">
-          {timer}
-          </div>
+          <div className="panelTimer col-xs-5 timeLg">{timer}</div>
           <div className="panelTaller">
             <Workshops />
           </div>
         </div>
         <div className="timeXs">
-        <div className="panelTimer col-xs-9 ">
-            {timer}
-          </div>
+          <div className="panelTimer col-xs-9 ">{timer}</div>
         </div>
 
-        <div className="panelSponsor">
-          {<CorouselSponsors />}
-        </div>
+        <div className="panelSponsor">{<CorouselSponsors />}</div>
       </div>
     </>
   );
